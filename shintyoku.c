faf45419370a5cc616e2d11e284c53f82afd6acc
vicowara <mm.souichi@gmail.com>
2017-10-12 22:56:21 +0900

    clean syntax

--- shintyoku.c
+++ shintyoku.c
@@ -1,3 +1,5 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h> // alloc_chrdev_region
@@ -13,28 +15,27 @@ static struct cdev c_dev; // キャラクタデバイス構造体
 
 // 内部バッファ
 #define MAX_BUFLEN 64
-unsigned char cdev_buf[ MAX_BUFLEN ];
-static int cdev_buflen = 0;
-
+static unsigned char cdev_buf[MAX_BUFLEN];
+static int cdev_buflen;
 
 // スペシャルファイルをオープンした
-static int cdev_open( struct inode *inode, struct file *filp ) 
+static int cdev_open(struct inode *inode, struct file *filp)
 {
-	printk( KERN_INFO "cdev_open\n" );
+	pr_info("cdev_open\n");
 	return 0;
 }
 
 
 // スペシャルファイルをクローズした
-static int cdev_release( struct inode* inode, struct file* filp )
+static int cdev_release(struct inode *inode, struct file *filp)
 {
-	printk( KERN_INFO "cdev_release\n" );
+	pr_info("cdev_release\n");
 	return 0;
 }
 
 
 // スペシャルファイルからの読み込み
-static ssize_t cdev_read( struct file* filp, char* buf, size_t count, loff_t* offset )
+static ssize_t cdev_read(struct file *filp, char *buf, size_t count, loff_t *offset)
 {
 	int len;
 	char text[MAX_BUFLEN];
@@ -44,10 +45,10 @@ static ssize_t cdev_read( struct file* filp, char* buf, size_t count, loff_t* of
 	} else {
 		len = sprintf(text, "damedesu\n");
 	}
-	printk( KERN_INFO "cdev_read count = %lu\n", count );
+	pr_info("cdev_read count = %lu\n", count);
 
-	if( copy_to_user( buf, text, len ) ){
-		printk( KERN_WARNING "copy_to_user failed\n" );
+	if (copy_to_user(buf, text, len)) {
+		pr_warn("copy_to_user failed\n");
 		return -EFAULT;
 	}
 
@@ -59,19 +60,21 @@ static ssize_t cdev_read( struct file* filp, char* buf, size_t count, loff_t* of
 
 
 // スペシャルファイルへの書き込み
-static ssize_t cdev_write( struct file* filp, const char* buf, size_t count, loff_t* offset )
+static ssize_t cdev_write(struct file *filp, const char *buf, size_t count, loff_t *offset)
 {
-	if( count >= MAX_BUFLEN ) return -EFAULT;
-
-	printk( KERN_INFO "cdev_write count = %lu\n", count );
-
-	if( copy_from_user( cdev_buf, buf, count ) ){
-		printk( KERN_WARNING "copy_from_user failed\n" );
+	if (count >= MAX_BUFLEN) {
 		return -EFAULT;
 	}
 
-	cdev_buf[ count ] = '\0';
-	printk( KERN_INFO "cdev_buf = %s\n", cdev_buf );
+	pr_info("cdev_write count = %lu\n", count);
+
+	if (copy_from_user(cdev_buf, buf, count)) {
+		pr_warn("copy_from_user failed\n");
+		return -EFAULT;
+	}
+
+	cdev_buf[count] = '\0';
+	pr_info("cdev_buf = %s\n", cdev_buf);
 
 	*offset += count;
 	cdev_buflen = count;
@@ -82,7 +85,7 @@ static ssize_t cdev_write( struct file* filp, const char* buf, size_t count, lof
 
 // ファイルオペレーション構造体
 // スペシャルファイルに対して読み書きなどを行ったときに呼び出す関数を登録する
-static struct file_operations modtest_fops ={
+static const struct file_operations modtest_fops = {
 	.owner   = THIS_MODULE,
 	.open    = cdev_open,
 	.release = cdev_release,
@@ -92,58 +95,59 @@ static struct file_operations modtest_fops ={
 
 
 // モジュール初期化
-static int __init modtest_module_init( void )
+static int __init modtest_module_init(void)
 {
 	int ret;
 
 	// キャラクタデバイス番号の動的取得
-	ret = alloc_chrdev_region( &dev_id, // 最初のデバイス番号が入る
+	ret = alloc_chrdev_region(
+			&dev_id, // 最初のデバイス番号が入る
 			0,  // マイナー番号の開始番号
 			MINOR_COUNT, // 取得するマイナー番号数
 			MODNAME // モジュール名
 			);
-	if( ret < 0 ){
-		printk( KERN_WARNING "alloc_chrdev_region failed\n" );
+	if (ret < 0) {
+		pr_warn("alloc_chrdev_region failed\n");
 		return ret;
 	}
 
 	// キャラクタデバイス初期化
 	// ファイルオペレーション構造体の指定もする
-	cdev_init( &c_dev, &modtest_fops );
+	cdev_init(&c_dev, &modtest_fops);
 	c_dev.owner = THIS_MODULE;
 
 	// キャラクタデバイスの登録
 	// MINOR_COUNT が 1 でマイナー番号の開始番号が 0 なので /dev/modtest0 が
 	// 対応するスペシャルファイルになる
-	ret = cdev_add( &c_dev, dev_id, MINOR_COUNT );
-	if( ret < 0 ){
-		printk( KERN_WARNING "cdev_add failed\n" );
+	ret = cdev_add(&c_dev, dev_id, MINOR_COUNT);
+	if (ret < 0) {
+		pr_warn("cdev_add failed\n");
 		return ret;
 	}
 
-	printk( KERN_INFO "modtest is loaded\n" );
-	printk( KERN_INFO "major = %d\n", MAJOR( dev_id ) );
-	printk( KERN_INFO "minor = %d\n", MINOR( dev_id ) );
+	pr_info("modtest is loaded\n");
+	pr_info("major = %d\n", MAJOR(dev_id));
+	pr_info("minor = %d\n", MINOR(dev_id));
 
 	return 0;
 }
 
 
 //  モジュール解放
-static void __exit modtest_module_exit( void )
+static void __exit modtest_module_exit(void)
 {
 	// キャラクタデバイス削除
-	cdev_del( &c_dev );
+	cdev_del(&c_dev);
 
 	// デバイス番号の返却
-	unregister_chrdev_region( dev_id, MINOR_COUNT );
+	unregister_chrdev_region(dev_id, MINOR_COUNT);
 
-	printk( KERN_INFO "modtest is removed\n" );
+	pr_info("modtest is removed\n");
 }
 
 
-module_init( modtest_module_init );
-module_exit( modtest_module_exit );
+module_init(modtest_module_init);
+module_exit(modtest_module_exit);
 
-MODULE_DESCRIPTION( MODNAME );
-MODULE_LICENSE( "GPL2" );
+MODULE_DESCRIPTION(MODNAME);
+MODULE_LICENSE("GPL v2");
